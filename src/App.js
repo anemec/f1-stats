a4c2d65e5ba6e3fdcd9b69a86698d237ac3522d7
Andrew Nemec <andrewnemec2@gmail.com>
2022-10-19 19:52:37 -0500

    f1-stats

--- src/App.js
+++ src/App.js
@@ -1,31 +1,20 @@
-import React from 'react';
+import React from "react";
+import Navbar from "./components/Navbar";
+import Sidebar from "./components/Sidebar";
+import Home from "./pages/Home";
 
 function App() {
   return (
-    <div className="flex flex-col h-full items-center justify-center bg-gray-200 text-gray-700">
-      <div className="flex items-center">
-        <h1 className="text-6xl font-thin tracking-wider">Create React App + Tailwind CSS</h1>
+    <div className="flex h-full bg-gray-200 text-gray-700">
+      <div className="w-64 bg-slate-400 h-full">
+        <Sidebar />
       </div>
-      <p className="my-6 tracking-wide">
-        Edit <code>src/App.js</code> and save to reload.
-      </p>
-      <div className="mt-6 flex justify-center">
-        <a
-          className="uppercase hover:underline"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <a
-          className="ml-10 uppercase hover:underline"
-          href="https://tailwindcss.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Tailwind
-        </a>
+      <div className="flex flex-col w-full">
+        <div className="h-20">
+          <Navbar />
+        </div>
+        {/* BrowserRouter */}
+        <Home />
       </div>
     </div>
   );
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Navbar = () => {
+  return <div className="w-full h-full bg-slate-300">Navbar</div>;
+};
+
+export default Navbar;
